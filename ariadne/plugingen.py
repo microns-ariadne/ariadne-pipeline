ef44bf7ac9491805c57ab4334ae000eb91bc231a
mgohde <mgohde10@gmail.com>
2016-03-21 17:40:14 -0400

    Added preliminary support for more dependency types.
    Ariadne generates and schedules dependencies in two ways:
    
    1. As luigi classes. This is done only if the plugin developer overrides the files_modified() method of AriadneOp
    2. Inline with normal plugin execution. This is done otherwise to appease the luigi gods.

--- ariadne/plugingen.py
+++ ariadne/plugingen.py
@@ -1,6 +1,7 @@
 # plugingen.py -- Tools to generate plugins as luigi modules.
 
 import plugin
+import tools
 import os
 import sys
 import datetime
@@ -11,24 +12,83 @@ def genheader(f):
     f.write("import luigi\nimport os\n")
 
 
-def gen_deps_kludge(deplist, f, plugindir):
-    """A quick kludge to get dependencies working until luigi's complete() method can be
-       worked around"""
-
+def gen_deps_inline(deplist, f, plugindir):
     if deplist==None:
         return
 
     for d in deplist:
-        f.write("        os.system('ariadne.py runplugin %s %s " % (d.dependency_name, plugindir))
-        for a in d.arg_dict:
-            f.write("%s=%s " % (a, str(d.arg_dict[a])))
+        f.write("        os.system('ariadne.py runplugin %s %s " % (d[2], plugindir))
+        for a in d[1]:
+            f.write("%s=%s " % (a, str(d[1][a])))
         f.write("')\n")
-            
+
+
+def write_dep_def(f, plugin, depnum, argdict, plugindir):
+    """Implements a limited subset of gen() intended to wrap individual dependencies."""
+    namestr="%s_l%d" % (plugin.name, depnum)
+
+    f.write("class %s(luigi.Task):\n\n\n" % namestr)
+    f.write("   def output(self):\n")
+    f.write("       return")
+    modlist=plugin.files_modified()
+    first=None
+
+    for f in modlist:   
+        if first==None:
+            first=f
+        else:
+            f.write(", ")
+        f.write("luigi.LocalTarget('%s')" % f)
+
+    f.write("\n\n\n")
+
+    f.write("   def run(self):\n")
+    f.write("       os.system('ariadne.py %splugin %s %s" % (exectype, wrappername, plugindir))
+
+    for a in argdict:
+        f.write(" %s=%s" % (a, argdict[a]))
+    f.write("')\n\n\n")
+
+    return namestr
+
+
+def parse_deps(plugin, f, plugindir, args):
+    deplist=plugin.depends()
+    classlist=[]
+    inlinelist=[]
+
+    depnum=0
+
+    if deplist!=None:
+        for d in deplist:
+            dclass=plugins.search_plugins(d.dependency_name)
+            if dclass==None:
+                print("Couldn't find plugin for dependency: %s" % d.dependency_name)
+                raise Exception
+            else:
+                # Determine if this dependency would be best suited
+                # for inline execution:
+                dep=dclass()
+
+                (a, b)=parse_deps(ddep, f, plugindir, d.arg_dict)
+                classlist.extend(a)
+                inlinelist.extend(b)
+
+    if plugin.files_modified() == []:
+        inline.append((dep, args, dep.name))
+    else:
+        gen_name=write_dep_def(f, dep, depnum, d.arg_dict, plugindir)
+        classlist.append((dep, args, gen_name))
+
+    return (classlist, inlinelist)
 
 
 def gen(plugin, f, wrappername, plugindir, exectype, existingargs):
     argnames=[]
     deps=[]
+
+    classdeplist=[]
+    inlinedeplist=[]
     
     try:
         argnames=plugin.get_arg_names()
@@ -36,42 +96,50 @@ def gen(plugin, f, wrappername, plugindir, exectype, existingargs):
         pass
 
     try:
-        deps=plugin.depends()
+        deplist=plugin.depends()
+        for d in deplist:
+            dclass=plugins.search_plugins(d.dependency_name)
+            if dclass==None:
+                print("Couldn't find plugin for dependency: %s" % d.dependency_name)
+                raise Exception
+            else:
+                (a, b)=parse_deps(dclass(), f, plugindir, d.arg_disct)
+                classdeplist.extend(a)
+                inlinedeplist.extend(b)
     except:
         pass
+
+    print("Number of class dependencies: %d" % len(classdeplist))
+    print("Number of inline dependencies: %d" % len(inlinedeplist))
     
     f.write("class %s_l(luigi.Task):\n" % wrappername)
     # Temporarily disabled for existingargs support:
     #for a in argnames:
     #    f.write("    %s=luigi.Parameter()\n" % a)
 
-    """
-    f.write("    def requires(self):\n")
-    f.write("        return")
-    if len(deps)>0:
-        for d in deps:
-            if d!=deps[0]:
-                f.write(",")
-            f.write(" %s_l(" % d.dependency_name)
+    if len(classdeplist):
+        f.write("   def requires(self):\n")
+        f.write("       return")
+        for c in classdeplist:
+            f.write(" %s(" % c[2])
             frst=None
-            for a in d.arg_dict:
+
+            for a in c[1]:
                 if frst==None:
                     frst=a
                 else:
                     f.write(",")
-                f.write("'%s'" % str(d.arg_dict[a]))
+
+                f.write("'%s'" % str(c[1][a]))
             f.write(")")
 
-    f.write("\n")
-    """
+        f.write("\n\n\n")
+    else:
+        print("No luigi-able dependencies found")
 
     f.write("    def run(self):\n")
-    gen_deps_kludge(deps, f, plugindir)
+    gen_deps_inline(inlinedeplist, f, plugindir)
     f.write("        os.system('ariadne.py %splugin %s %s" % (exectype, wrappername, plugindir))
-
-    # Temporarily disabled for existingargs support:
-    #for a in argnames:
-    #    f.write(" "+a+"="+"'+self."+a+"+'")
 
     for e in existingargs:
         f.write(" %s" % e)

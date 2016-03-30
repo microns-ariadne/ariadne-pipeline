22f8fde6d8fc7d568baa6118ba08e27fa72f1597
mgohde <mgohde10@gmail.com>
2016-03-30 17:24:00 -0600

    (hopefully) fixed install script.

--- plugins/old/hdf5vtools.py
+++
@@ -1,6 +0,0 @@
-# hdf5vtools.py -- A ValidationPlugin to check various aspects of an hdf5 dataset.
-
-import ariadneplugin
-import ariadnetools
-import h5py
-import os
--- setup.py
+++ setup.py
@@ -9,12 +9,19 @@ from setuptools import setup, Extension, find_packages
 import os
 import sys
 
+plugins=os.listdir("plugins/")
+newpllist=[]
+
+for p in plugins:
+    newpllist.append("plugins/%s" % p)
+
 setup(
     name='ariadne-pipeline',
-    version='1.0a8',
+    version='1.0a9',
     description="Run, and manage machine learning pipelines.",
-    scripts=['ariadne/ariadne.py', 'scripts/ariadne', 'ariadne/shell2pipe.py', 'scripts/ariadne-download.sh'],
+    scripts=['scripts/ariadne', 'ariadne/shell2pipe.py', 'scripts/ariadne-download.sh'],
     include_package_data=True,
     install_requires=['h5py', 'nose', 'luigi'],
-    packages=find_packages(exclude=['tests', 'scripts', 'bin']),
+    packages=find_packages(exclude=['tests', 'scripts', 'bin', 'plugins']),
+    data_files=[('plugins', newpllist)], # Allow plugins to be copied verbatim.
     )
